176cfa5d418c4ca08701075f6fc3c2c4d5dd3990
Vansh Bhatnagar <vanshbhatnagar12@gmail.com>
2024-06-09 22:20:21 +0530

    backend: add signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,8 +32,24 @@ catch(e) {
    return c.json({error: "error while signing up"})
 }
 })
-app.post('/api/v1/user/signin' , (c) => {
-  return c.text('hello ')
+app.post('/api/v1/user/signin' , async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email
+    }
+  })
+  if(!user) {
+    c.status(403)
+    return c.json({
+      error: "Specified user does not exists"
+    })
+  }
+  const jwt = await sign({id: user.id},c.env.JWT_SECRET)
+  return c.json({jwt})
 })
 app.post('/api/v1/blog' , (c) => {
   return c.text('')
