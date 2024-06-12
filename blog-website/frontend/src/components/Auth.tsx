e6b0b68e81b5a93b3ffa88183881086fbc2eaac9
Vansh Bhatnagar <vanshbhatnagar12@gmail.com>
2024-06-12 23:16:43 +0530

    frontend: add optional password 'type' to the input box

--- blog-website/frontend/src/components/Auth.tsx
+++ blog-website/frontend/src/components/Auth.tsx
@@ -16,16 +16,18 @@ export const Auth =({type} : {type: "signup" | "signin"}) => {
                 Create an Account
         </div>
         <div className="flex" >
-            Already have an account?  <div className="pl-2"><Link to={"/signin"}>Login</Link></div>
-            
+         <div className=" text-slate-500">
+            {type === "signin" ? "Don't have an account?" : "Already have an account?" }
+                <Link className="underline pl-1" to={type === "signin" ? "/signup" : "/signin"}>{type === "signin" ? "Sign up" : "Sign in"}</Link>
+                </div>
         </div>
         <div>
-        <LabelledInput label="Name" placeholder="John" onChange={(e) => {
+       <LabelledInput label="Name" placeholder="John" onChange={(e) => {
             setPostInputs({
                 ...postInputs, //...postInputs directly just copies previous values to new values and 
                 name: e.target.value //nai values ko update kardeta hai
             })
-        }}/>
+        }}/> 
         </div>
         <div><LabelledInput label="Username" placeholder="john@1234" onChange={(e) => {
             setPostInputs({
@@ -33,7 +35,7 @@ export const Auth =({type} : {type: "signup" | "signin"}) => {
                username: e.target.value //nai values ko update kardeta hai
             })
         }}/></div>
-        <div><LabelledInput label="Password" placeholder="passkey123" onChange={(e) => {
+        <div><LabelledInput label="Password" type = {"password"} placeholder="passkey123" onChange={(e) => {
             setPostInputs({
                 ...postInputs, //...postInputs directly just copies previous values to new values and 
                 password: e.target.value //nai values ko update kardeta hai
@@ -48,12 +50,13 @@ interface LabelledInputType {
     label : string;
     placeholder:string;
     onChange: any;
+    type? : string;
 }
-function LabelledInput({label,placeholder,onChange} : LabelledInputType){
+function LabelledInput({label,placeholder,onChange,type} : LabelledInputType){
 return <div>
     <div>
             <label onChange= {onChange} for="title" class="block mb-2 text-sm font-bold text-black-500 mt-5">{label}</label>
-            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder={placeholder} required />
+            <input id="first_name" type= {type || "text"} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder={placeholder} required />
 
         </div>
 </div>
