0ca5216c4fe62da2ab52f5c644243aec474bf693
Vansh Bhatnagar <vanshbhatnagar12@gmail.com>
2024-06-14 18:45:50 +0530

    frontend: introduce custom hooks

---
+++ blog-website/frontend/src/hooks/index.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { BACKEND_URL } from "../config";
+
+export interface Blog {
+    "content": string;
+    "title": string;
+    "id": number;
+    "author": {
+        "name": string
+    }
+}
+//custom hooks for displaying all blogs
+export const useBlogs = () => {
+    const [blogs,setBlogs] = useState<Blog[]>([]);
+    useEffect(() =>{
+        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,{
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        }).then(response => {
+            setBlogs(response.data.blogs);
+        })
+    },[])
+    return {
+        blogs //state variable is getting returned
+    }
+}
