cc3c530b9df922215f436aaa5ec34e83270005ec
Vansh Bhatnagar <vanshbhatnagar12@gmail.com>
2024-06-15 11:19:40 +0530

    add routing for blog components and pages

--- blog-website/frontend/src/pages/Blog.tsx
+++ blog-website/frontend/src/pages/Blog.tsx
@@ -1,5 +1,22 @@
+import { useParams } from "react-router-dom";
+import { useBlog } from "../hooks"
+import { Appbar } from "../components/Appbar";
+import { FullBlog } from "../components/FullBlog";
+
 export const Blog = () => {
+    const {id} = useParams(); //get dynamic route parameters
+    const {loading,blog} = useBlog({
+        id: id || ""
+    });
+    if(loading) {
+        return <div>
+            <div><Appbar/></div>
+            <div className="flex justify-center">
+            loading the content please wait
+        </div>
+        </div>
+    }
     return <div>
-        blog
+        <FullBlog blog={blog}/>
     </div>
 }
